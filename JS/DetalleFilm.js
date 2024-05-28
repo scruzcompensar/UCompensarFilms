f4c99ce3c2583bce8c93247dedeb93f25237c1c2
scruz <santiagocruz@ucompensar.edu.co>
2024-05-28 17:11:59 -0500

    Ajuste rutas de archivos

--- JS/DetalleFilm.js
+++ JS/DetalleFilm.js
@@ -8,7 +8,7 @@ const datosPeliculas = [
          reparto4: "Idris Elba ", reparto3ptr: "Heimdall",reparto3Img: "https://tenor.com/es-419/view/approve-wink-smile-idris-elba-thumbs-up-gif-7331500.gif", 
          reparto5: "Stellan Skarsgård", reparto5ptr: "Erik Selvig",reparto5Img: "https://tenor.com/es-419/view/hey-wave-hello-whats-up-smile-gif-16099259.gif",
          fondo: "https://tenor.com/es-419/view/thor-mjolnir-cheer-mcu-chris-hemsworth-gif-15698683.gif",
-         foto: "/Imagenes/Thor2.jpg"},
+         foto: "../Imagenes/Thor2.jpg"},
     { id: "2", nombre: "Iron Man 3", contenido: "El director Shane Black (Kiss Kiss Bang Bang) nos trae Iron Man 3, la nueva aventura de la saga basada en la conocida serie de cómics de Marvel, en la que el multimillonario Tony Stark/ Iron Man (Robert Downey Jr., Marvel Los Vengadores), vuelve a enfrentarse a las fuerzas del mal con su letal armadura.",
         director: "Shane Black", escritor: "Drew Pearce", presupuesto: "200", recaudacion: "+1.215", nominaciones: "+15", aprobacion: "79%",
          reparto1: "Robert Downey Jr", reparto1ptr: "Iron Man", reparto1Img: "https://tenor.com/es-419/view/rdj-robert-downey-jr-marvel-iron-man-meme-gif-27073496.gif", 
@@ -17,7 +17,7 @@ const datosPeliculas = [
          reparto4: "Ben Kingsley ", reparto4ptr: "Mandarin",reparto4Img: "https://tenor.com/es-419/view/iron-man3-action-mandarin-ben-kingsley-gif-3430903.gif", 
          reparto5: "Guy Pearce", reparto5ptr: "Aldrich Killian",reparto5Img: "https://tenor.com/es-419/view/guy-pearce-gaze-look-man-gif-9072163.gif",
          fondo: "https://tenor.com/es-419/view/iron-man-tony-stark-robert-downey-jr-marvel-superhero-gif-16430080.gif",
-         foto: "/Imagenes/Iron3.webp"},
+         foto: "../Imagenes/Iron3.webp"},
     { id: "3", nombre: "Thor", contenido: "Thor (Chris Hemsworth, Los vengadores), príncipe del reino de Asgard, es un poderoso y arrogante guerrero cuya imprudencia desencadena antiguas rencillas en el dominio. Por ello, su padre Odín le envía al planeta Tierra a modo de castigo para que viva como un humano y descubra así el valor de la humildad. ",
          director: "Kenneth Branagh", escritor: "Ashley Edward Miller", presupuesto: "140", recaudacion: "+449", nominaciones: "+23", aprobacion: "77%",
          reparto1: "Chris Hemsworth", reparto1ptr: "Thor", reparto1Img: "https://tenor.com/es-419/view/okay-yeah-ok-chris-hemsworth-sure-ok-gif-15747035.gif", 
@@ -26,7 +26,7 @@ const datosPeliculas = [
          reparto4: "Idris Elba ", reparto3ptr: "Heimdall",reparto3Img: "https://tenor.com/es-419/view/approve-wink-smile-idris-elba-thumbs-up-gif-7331500.gif", 
          reparto5: "Stellan Skarsgård", reparto5ptr: "Erik Selvig",reparto5Img: "https://tenor.com/es-419/view/hey-wave-hello-whats-up-smile-gif-16099259.gif",
          fondo: "https://tenor.com/es-419/view/thor-gif-21966792.gif",
-         foto: "/Imagenes/Thor1.webp"},
+         foto: "../Imagenes/Thor1.webp"},
     { id: "4", nombre: "Avengers - Infinity War", contenido: "Un nuevo peligro acecha procedente de las sombras del cosmos. Thanos, el infame tirano intergaláctico, tiene como objetivo reunir las seis Gemas del Infinito, artefactos de poder inimaginable, y usarlas para imponer su perversa voluntad a toda la existencia. Los Vengadores y sus aliados tendrán que luchar contra el mayor villano al que se han enfrentado nunca.",
          director: "Anthony Russo, Joe Russo", escritor: "Christopher Markus", presupuesto: "400", recaudacion: "+2.052", nominaciones: "+25", aprobacion: "85%",
          reparto1: "Robert Downey Jr", reparto1ptr: "Iron Man", reparto1Img: "https://tenor.com/es-419/view/rdj-robert-downey-jr-marvel-iron-man-meme-gif-27073496.gif", 
@@ -35,7 +35,7 @@ const datosPeliculas = [
          reparto4: "Tom Hiddleston", reparto4ptr: "Loki",reparto4Img: "https://tenor.com/es-419/view/tom-hiddleston-gif-22793598.gif", 
          reparto5: "Scarlett Johansson", reparto5ptr: "Viuda Negra",reparto5Img: "https://tenor.com/es-419/view/flirt-flirty-gif-27167000.gif",
          fondo: "https://tenor.com/es-419/view/running-flying-avengers-black-panther-captain-america-gif-20283634.gif",
-         foto: "/Imagenes/Avengers3.webp"},
+         foto: "../Imagenes/Avengers3.webp"},
     { id: "5", nombre: "Thor - Ragnarok", contenido: "Asgard se encuentra en manos de una poderosa amenaza, la despiadada y todopoderosa Hela (Cate Blanchett), que ha robado el trono y ha encarcelado a Thor (Chris Hemsworth), enviándole como prisionero hasta el otro extremo de la galaxia. Sin su martillo, el mítico y poderoso Mjölnir, el Dios del Trueno se encontrará a sí mismo en una carrera contra el tiempo.",
          director: "Taika Waititi", escritor: "Eric Pearson", presupuesto: "180", recaudacion: "+855", nominaciones: "+25", aprobacion: "93%",
          reparto1: "Chris Hemsworth", reparto1ptr: "Thor", reparto1Img: "https://tenor.com/es-419/view/okay-yeah-ok-chris-hemsworth-sure-ok-gif-15747035.gif", 
@@ -44,7 +44,7 @@ const datosPeliculas = [
          reparto4: "Idris Elba ", reparto3ptr: "Heimdall",reparto3Img: "https://tenor.com/es-419/view/approve-wink-smile-idris-elba-thumbs-up-gif-7331500.gif", 
          reparto5: "Jeff Goldblum", reparto5ptr: "Erik Selvig",reparto5Img: "https://tenor.com/es-419/view/goldblum-heart-gif-13386722.gif",
          fondo: "https://tenor.com/es-419/view/thor-ragnarok-hulk-thor-valkerie-loki-gif-16648113.gif",
-         foto: "/Imagenes/Thor3.webp"},
+         foto: "../Imagenes/Thor3.webp"},
     { id: "6", nombre: "The Avengers", contenido: "Cuando un enemigo inesperado amenaza la seguridad del planeta y de sus habitantes, Nick Fury (Samuel L. Jackson), director de SHIELD, monta un dispositivo con todos los hombres capaces de preservar a la humanidad del caos. El enemigo es tan poderoso, que necesita que todos los superhéroes luchen juntos y formen un equipo compacto. Ellos serán: el Capitán América (Chris Evans), Thor (Chris Hemsworth), Iron Man (Robert Downey Jr.), Hulk (Mark Ruffalo), Ojo de Halcón (Jeremy Renner) y la Viuda Negra (Scarlett Johansson)",
          director: "Joss Whedon", escritor: "Joss Whedon, Zak Penn", presupuesto: "220", recaudacion: "+1.520", nominaciones: "+15", aprobacion: "91%",
          reparto1: "Robert Downey Jr", reparto1ptr: "Iron Man", reparto1Img: "https://tenor.com/es-419/view/rdj-robert-downey-jr-marvel-iron-man-meme-gif-27073496.gif", 
@@ -53,7 +53,7 @@ const datosPeliculas = [
          reparto4: "Tom Hiddleston", reparto4ptr: "Loki",reparto4Img: "https://tenor.com/es-419/view/tom-hiddleston-gif-22793598.gif", 
          reparto5: "Scarlett Johansson", reparto5ptr: "Viuda Negra",reparto5Img: "https://tenor.com/es-419/view/flirt-flirty-gif-27167000.gif",
          fondo: "https://tenor.com/es-419/view/avengers-ready-lets-do-this-iron-man-tony-stark-gif-17148023.gif",
-         foto: "/Imagenes/Avengers.png"},
+         foto: "../Imagenes/Avengers.png"},
     { id: "7", nombre: "Avengers - Era de Ultrón", contenido: "El destino del planeta pende de un hilo cuando Tony Stark intenta hacer funcionar un inactivo programa para mantener la paz. Las cosas le salen mal y los héroes más poderosos, incluyendo Iron Man, Capitán América, la Viuda Negra, Thor, el Increíble Hulk y Ojo de Halcón, se ven enfrentados a la prueba definitiva.",
          director: "Joss Whedon", escritor: "Joss Whedon", presupuesto: "316", recaudacion: "+1.405", nominaciones: "+28", aprobacion: "75%",
          reparto1: "Robert Downey Jr", reparto1ptr: "Iron Man", reparto1Img: "https://tenor.com/es-419/view/rdj-robert-downey-jr-marvel-iron-man-meme-gif-27073496.gif", 
@@ -62,7 +62,7 @@ const datosPeliculas = [
          reparto4: "Tom Hiddleston", reparto4ptr: "Loki",reparto4Img: "https://tenor.com/es-419/view/tom-hiddleston-gif-22793598.gif", 
          reparto5: "Scarlett Johansson", reparto5ptr: "Viuda Negra",reparto5Img: "https://tenor.com/es-419/view/flirt-flirty-gif-27167000.gif",
          fondo: "https://tenor.com/es-419/view/ultron-marvel-robot-artificial-intelligence-avengers-age-of-ultron-gif-16652736.gif",
-         foto: "/Imagenes/Avengers2.webp"},
+         foto: "../Imagenes/Avengers2.webp"},
     { id: "8", nombre: "Iron Man", contenido: "Tony Stark (Robert Downey Jr., 'Chaplin'), dueño de Industrias Stark, inventor consagrado, vendedor de armas y playboy multimillonario, es secuestrado en Afganistán después de una demostración armamentística para el Ejército de los Estados Unidos. Forzado por sus captores a fabricar un arma temible, acaba construyendo en secreto una armadura de alta tecnología revolucionaria que usa para escaparse.",
         director: "Jon Favreau", escritor: "Mark Fergus", presupuesto: "140", recaudacion: "+585", nominaciones: "+9", aprobacion: "94%",
         reparto1: "Robert Downey Jr", reparto1ptr: "Iron Man", reparto1Img: "https://tenor.com/es-419/view/rdj-robert-downey-jr-marvel-iron-man-meme-gif-27073496.gif", 
@@ -71,7 +71,7 @@ const datosPeliculas = [
         reparto4: "Jeff Bridges ", reparto4ptr: "Iron Monger",reparto4Img: "https://tenor.com/es-419/view/jeff-bridges-the-dude-big-lebowski-stir-gif-12134868.gif", 
         reparto5: "Clark Gregg", reparto5ptr: "Agente Phil Coulson",reparto5Img: "https://tenor.com/es-419/view/captain-marvel-agent-coulson-phil-coulson-clark-gregg-marvel-gif-16091869.gif",
          fondo: "https://tenor.com/es-419/view/donut-iron-man-tony-stark-hungry-bite-gif-15065412.gif",
-         foto: "/Imagenes/Iron1.jpg"},
+         foto: "../Imagenes/Iron1.jpg"},
     { id: "9", nombre: "Avengers - End Game", contenido: "Después de los devastadores eventos ocurridos en Vengadores: Infinity War, el universo está en ruinas debido a las acciones de Thanos, el Titán Loco. Tras la derrota, las cosas no pintan bien para los Vengadores. Mientras Iron Man (Robert Downey Jr.) vaga en soledad junto a Nebula (Karen Gillan) en una nave lejos de la Tierra, el grupo encabezado por Capitán América (Chris Evans), Viuda Negra (Scarlett Johansson), Hulk (Mark Ruffalo) y Thor (Chris Hemsworth) deberá tratar de revertir los efectos de la catástrofe provocada por Thanos. Los Vengadores deberán reunirse una vez más para deshacer sus acciones y restaurar el orden en el universo de una vez por todas. Esta vez, contarán también con aliados como Ojo de Halcón (Jeremy Renner) y Capitana Marvel (Brie Larson), además de Ant-Man (Paul Rudd), que presumiblemente podría haber estado atrapado en el Reino Cuántico. Juntos, se prepararán para la batalla final, sin importar cuáles sean las consecuencias.",
         director: "Anthony Russo, Joe Russo", escritor: "Christopher Markus", presupuesto: "400", recaudacion: "+2.799", nominaciones: "+35", aprobacion: "94%",
          reparto1: "Robert Downey Jr", reparto1ptr: "Iron Man", reparto1Img: "https://tenor.com/es-419/view/rdj-robert-downey-jr-marvel-iron-man-meme-gif-27073496.gif", 
@@ -80,7 +80,7 @@ const datosPeliculas = [
          reparto4: "Tom Hiddleston", reparto4ptr: "Loki",reparto4Img: "https://tenor.com/es-419/view/tom-hiddleston-gif-22793598.gif", 
          reparto5: "Scarlett Johansson", reparto5ptr: "Viuda Negra",reparto5Img: "https://tenor.com/es-419/view/flirt-flirty-gif-27167000.gif",
          fondo: "https://tenor.com/es-419/view/avengers-running-lets-do-this-gif-19919029.gif",
-         foto: "/Imagenes/Avengers4.png"},
+         foto: "../Imagenes/Avengers4.png"},
     { id: "10", nombre: "Iron Man 2", contenido: "Después de los sucesos ocurridos en Iron Man (2008), el mundo entero sabe que Tony Stark (Robert Downey Jr.) es el superhéroe enmascarado conocido como Iron Man. Tanto el gobierno, como la prensa y la opinión pública están haciendo campaña a favor de que Stark comparta su tecnología con el ejército, pero el creador de las Industrias Stark se niega a compartir sus secretos por el temor a que sus diseños caigan en manos de personas poco adecuadas y peligrosas.",
          director: "Jon Favreau", escritor: "Justin Theroux", presupuesto: "200", recaudacion: "+623", nominaciones: "+12", aprobacion: "72%",
          reparto1: "Robert Downey Jr", reparto1ptr: "Iron Man", reparto1Img: "https://tenor.com/es-419/view/rdj-robert-downey-jr-marvel-iron-man-meme-gif-27073496.gif", 
@@ -89,12 +89,12 @@ const datosPeliculas = [
          reparto4: "Scarlett Johansson", reparto4ptr: "Viuda Negra",reparto4Img: "https://tenor.com/es-419/view/flirt-flirty-gif-27167000.gif",
          reparto5: "Mickey Rourke", reparto5ptr: "Agente Phil Coulson",reparto5Img: "https://tenor.com/es-419/view/taking-shades-off-mickey-rourke-gary-night-walk-let-me-tell-you-something-gif-21415520.gif",
          fondo: "https://tenor.com/es-419/view/iron-man-and-war-machine-mask-on-gif-marvel-iron-man-war-machine-lets-go-gif-17879708.gif",
-         foto: "/Imagenes/Iron2.jpg"},
+         foto: "../Imagenes/Iron2.jpg"},
 ];
 
 
 document.getElementById("logo_img").addEventListener("click", function() {
-    window.location.href = "/HTML/Index.html";
+    window.location.href = "../HTML/Index.html";
 });
 
 // Obtener el ID de la tarjeta de la URL
--- JS/Index.js
+++ JS/Index.js
@@ -1,15 +1,15 @@
 // Datos para las tarjetas de peliculas
 const datosTarjetas = [
-    { id: 1, nombre: "Thor 2 - The Dark World", contenido: "Malekith, un enemigo más antiguo que el universo, regresa a la Tierra para cumplir su plan destructor.", tiempo: "112 mins", imagen: "/Imagenes/Thor2.jpg" ,fecha: "08/11/2013"},
-    { id: 2, nombre: "Iron Man 3", contenido: "El brillante Tony Stark, tras ver destruido todo su universo personal, debe encontrar y enfrentarse a un enemigo cuyo poder no conoce límites.", tiempo: "131 mins", imagen: "/Imagenes/Iron3.webp", fecha: "03/05/2013" },
-    { id: 3, nombre: "Thor", contenido: "Thor es desterrado a la Tierra por su padre para que viva entre los hombres y descubra así el verdadero sentido de la humildad.", tiempo: "115 mins", imagen: "/Imagenes/Thor1.webp",fecha: "06/05/2011"},
-    { id: 4, nombre: "Avengers - Infinity War", contenido: "Los superhéroes se alían para vencer al poderoso Thanos, el peor enemigo al que se han enfrentado.", tiempo: "149 mins", imagen: "/Imagenes/Avengers3.webp", fecha: "04/05/2012"},
-    { id: 5, nombre: "Thor - Ragnarok", contenido: "Thor está preso en el otro extremo del universo. Necesita regresar a tiempo para evitar que la todopoderosa Hela destruya su mundo.", tiempo: "130 mins", imagen: "/Imagenes/Thor3.webp",fecha: "03/11/2017" },
-    { id: 6, nombre: "Avengers", contenido: "El director de la Agencia SHIELD decide reclutar a un equipo para salvar al mundo de un desastre casi seguro.", tiempo: "143 mins", imagen: "/Imagenes/Avengers.png",fecha: "04/05/2012" },
-    { id: 7, nombre: "Avengers - Era de Ultrón", contenido: "Los Vengadores se reúnen de nuevo y juntan sus fuerzas con las de los recién llegados Quicksilver y Bruja Escarlata.", tiempo: "141 mins", imagen: "/Imagenes/Avengers2.webp" ,fecha: "01/05/2015"},
-    { id: 8, nombre: "Iron Man", contenido: "Un empresario millonario construye un traje blindado y lo usa para combatir el crimen y el terrorismo.", tiempo: "126 mins", imagen: "/Imagenes/Iron1.jpg" ,fecha: "02/05/2008"},
-    { id: 9, nombre: "Avengers - End Game", contenido: "Los Vengadores restantes deben encontrar una manera de recuperar a sus aliados para un enfrentamiento épico con Thanos.", tiempo: "181 mins", imagen: "/Imagenes/Avengers4.png" ,fecha: "26/05/2019"},
-    { id: 10, nombre: "Iron Man 2", contenido: "Con el mundo consciente de que él es Iron Man, el millonario Tony Stark debe confrontar a un enemigo nuevo y poderoso.", tiempo: "124 mins", imagen: "/Imagenes/Iron2.jpg" ,fecha: "07/05/2010"},
+    { id: 1, nombre: "Thor 2 - The Dark World", contenido: "Malekith, un enemigo más antiguo que el universo, regresa a la Tierra para cumplir su plan destructor.", tiempo: "112 mins", imagen: "../Imagenes/Thor2.jpg" ,fecha: "08/11/2013"},
+    { id: 2, nombre: "Iron Man 3", contenido: "El brillante Tony Stark, tras ver destruido todo su universo personal, debe encontrar y enfrentarse a un enemigo cuyo poder no conoce límites.", tiempo: "131 mins", imagen: "../Imagenes/Iron3.webp", fecha: "03/05/2013" },
+    { id: 3, nombre: "Thor", contenido: "Thor es desterrado a la Tierra por su padre para que viva entre los hombres y descubra así el verdadero sentido de la humildad.", tiempo: "115 mins", imagen: "../Imagenes/Thor1.webp",fecha: "06/05/2011"},
+    { id: 4, nombre: "Avengers - Infinity War", contenido: "Los superhéroes se alían para vencer al poderoso Thanos, el peor enemigo al que se han enfrentado.", tiempo: "149 mins", imagen: "../Imagenes/Avengers3.webp", fecha: "04/05/2012"},
+    { id: 5, nombre: "Thor - Ragnarok", contenido: "Thor está preso en el otro extremo del universo. Necesita regresar a tiempo para evitar que la todopoderosa Hela destruya su mundo.", tiempo: "130 mins", imagen: "../Imagenes/Thor3.webp",fecha: "03/11/2017" },
+    { id: 6, nombre: "Avengers", contenido: "El director de la Agencia SHIELD decide reclutar a un equipo para salvar al mundo de un desastre casi seguro.", tiempo: "143 mins", imagen: "../Imagenes/Avengers.png",fecha: "04/05/2012" },
+    { id: 7, nombre: "Avengers - Era de Ultrón", contenido: "Los Vengadores se reúnen de nuevo y juntan sus fuerzas con las de los recién llegados Quicksilver y Bruja Escarlata.", tiempo: "141 mins", imagen: "../Imagenes/Avengers2.webp" ,fecha: "01/05/2015"},
+    { id: 8, nombre: "Iron Man", contenido: "Un empresario millonario construye un traje blindado y lo usa para combatir el crimen y el terrorismo.", tiempo: "126 mins", imagen: "../Imagenes/Iron1.jpg" ,fecha: "02/05/2008"},
+    { id: 9, nombre: "Avengers - End Game", contenido: "Los Vengadores restantes deben encontrar una manera de recuperar a sus aliados para un enfrentamiento épico con Thanos.", tiempo: "181 mins", imagen: "../Imagenes/Avengers4.png" ,fecha: "26/05/2019"},
+    { id: 10, nombre: "Iron Man 2", contenido: "Con el mundo consciente de que él es Iron Man, el millonario Tony Stark debe confrontar a un enemigo nuevo y poderoso.", tiempo: "124 mins", imagen: "../Imagenes/Iron2.jpg" ,fecha: "07/05/2010"},
 ];
 
 // Función para mostrar las tarjetas
@@ -144,7 +144,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 //Evento para redireccionar a pagina principal al dar clik en icono
 document.getElementById("logo_img").addEventListener("click", function() {
-    window.location.href = "/HTML/Index.html";
+    window.location.href = "../HTML/Index.html";
 });
 
 //Evento de acción para filtro de aleatorio
